364ed8f3e2d2f9dd74c702e251e9b344720da1e9
Jared Bard <jabard89@gmail.com>
2025-05-01 16:09:24 -0500

    small change to docker

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -1,6 +1,6 @@
 container="spoc:v1"
-repo_dir="/Users/jbard/Library/CloudStorage/OneDrive-TexasA&MUniversity/repos/SPOC"
-input_dir="/Users/jbard/Library/CloudStorage/OneDrive-TexasA&MUniversity/repos/SPOC/example/spoctest_GNAS2_GP119_v3"
+repo_dir="/Users/jbard/Library/CloudStorage/OneDrive-TexasA&MUniversity/repos/bardlab_SPOC"
+input_dir="/Users/jbard/Library/CloudStorage/OneDrive-TexasA&MUniversity/repos/bardlab_SPOC/example/spoctest_GNAS2_GP119_v3"
 
 docker run --rm \
   -v "${input_dir}":/input \
